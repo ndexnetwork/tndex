45d37787d0232b2c0665fa04847b53fa3b93bec0
BitBD <tanvirtex@gmail.com>
2020-04-28 09:28:15 +0600

    forget to add js

---
+++ html/www/js/data/remotenodesbootstrap.testnet.js
@@ -0,0 +1,44 @@
+/******************************************************************************
+ * Copyright © 2013-2016 The Nxt Core Developers.                             *
+ * Copyright © 2016-2019 Jelurida IP B.V.                                     *
+ *                                                                            *
+ * See the LICENSE.txt file at the top-level directory of this distribution   *
+ * for licensing information.                                                 *
+ *                                                                            *
+ * Unless otherwise agreed in a custom licensing agreement with Jelurida B.V.,*
+ * no part of the Nxt software, including this file, may be copied, modified, *
+ * propagated, or distributed except according to the terms contained in the  *
+ * LICENSE.txt file.                                                          *
+ *                                                                            *
+ * Removal or modification of this copyright notice is prohibited.            *
+ *                                                                            *
+ ******************************************************************************/
+
+//TODO: manually add bootstrap nodes
+RemoteNodesManager.prototype.REMOTE_NODES_BOOTSTRAP =
+{
+    "peers": [
+             {
+                "downloadedVolume":0,
+                "address":"155.138.142.58",
+                "inbound":true,
+                "blockchainState":"UP_TO_DATE",
+                "uploadedVolume":0,
+                "services":[
+                    "API",
+                    "CORS"
+                ],
+                "version":"2.0.2",
+                "platform":"Linux amd64",
+                "lastUpdated":0,
+                "blacklisted":false,
+                "announcedAddress":"test.npay.life",
+                "apiPort":6867,
+                "application":"nDEX",
+                "port":6898,
+                "lastConnectAttempt":0,
+                "state":2,
+                "shareAddress":true
+            }
+    ]
+};
